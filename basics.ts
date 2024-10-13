6df7cc04745a81036a2ebc602f8d54f9bd7a7e41
ryanren <wenkun.ren@gmail.com>
2024-10-13 11:48:45 -0500

    React Complete Project

---
+++ basics.ts
@@ -0,0 +1,54 @@
+let age: number;
+
+age = 12;
+
+let userName: string | string[];
+
+userName = 'Max';
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+
+let hobbies: string[];
+
+hobbies = ['Sports','Cooking','Game'];
+
+type Person = {
+    name: String;
+    age: number;
+}
+
+let person: Person;
+
+// person: {
+//     name: string;
+//     age: number;
+// };
+
+person = {
+    name: 'Max',
+    age: 32
+};
+
+// person = {
+//     isEmployee: true
+// };
+
+let people: Person[];
+
+
+let course: string | number = 'React - the complete guide';
+
+course = 1243543;
+
+
+// functions & types
+
+
+function add(a: number, b: number){
+    return a + b;
+}
+
+
