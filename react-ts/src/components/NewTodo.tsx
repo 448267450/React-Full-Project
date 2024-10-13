3eefd4227f43a86e0d983f15959ce397067aeeec
ryanren <wenkun.ren@gmail.com>
2024-10-13 17:55:28 -0500

    useRef state method

---
+++ react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useRef } from "react";
+
+const NewTodo = () => {
+   const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+   const submitHandler = (event: React.FormEvent) => {
+       event.preventDefault();
+   };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo Text</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
